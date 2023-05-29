6bb173437df3454adb3be1c72665f9d0c7a073d2
andrii.m2 <amdrii.m@esenti.co>
2023-05-29 21:08:58 +0300

    add 572

---
+++ 572. Subtree of Another Tree/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	return root != nil && (isSameTree(root, subRoot) || isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot))
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if (p != nil && q != nil) && (p.Val == q.Val) {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(1)
+}
