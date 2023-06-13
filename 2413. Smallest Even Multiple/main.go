6935b9a3f7a36c0003b0e63737087952b675c989
andrii.m2 <amdrii.m@esenti.co>
2023-06-13 13:22:23 +0300

    add 2413

---
+++ 2413. Smallest Even Multiple/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func smallestEvenMultiple(n int) int {
+	if n%2 == 0 {
+		return n
+	}
+	return n * 2
+}
+
+func main() {
+	fmt.Println(smallestEvenMultiple(2))
+}
